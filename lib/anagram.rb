400265c76693154b9c09b9c5df9b5c0f6a509e43
Nicholas Levenson <nicklevenson@gmail.com>
2020-10-26 12:15:41 -0700

    commit tree 39cd601e14cb04777d43c07836b616df609f1d97

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    
+    def initialize(word)
+        @word = word
+    end
+    
+    def match(anagrams)
+    matches = []
+        anagrams.each do |gram|
+            if gram.split("").sort == word.split("").sort
+                matches << gram
+            end
+        end
+    matches
+    end
+
+
+end
